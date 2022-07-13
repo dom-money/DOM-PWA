9729b3b7afb3875e79a77e4377c7453f27e61cc9
Ihor Kubrysh <ihorku@dom.money>
2022-07-14 00:51:36 +0300

    Add Total Balance component & stories

---
+++ components/TotalBalance.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Title from './Title';
+import AmountDisplay from './AmountDisplay';
+
+interface TotalBalanceProps {
+  /**
+   * Currency amount
+   */
+  amount: number;
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TitleWithMargin = styled(Title)`
+  margin-bottom: 1.188rem;
+  margin-top: 1.875rem;
+`;
+
+const AmountDisplayWithMargin = styled(AmountDisplay)`
+  margin-bottom: 2.5rem;
+`;
+
+const TotalBalance = ({ amount }: TotalBalanceProps) => {
+  return (
+    <Wrapper>
+      <TitleWithMargin text='Total Balance'/>
+      <AmountDisplayWithMargin
+        amount={amount}
+        size='medium'
+      />
+    </Wrapper>
+  );
+};
+
+export default TotalBalance;
