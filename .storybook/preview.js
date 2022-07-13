dfc362a09b24dba468a80b0c93ce1cd054afeb42
Ihor Kubrysh <ihorku@dom.money>
2022-07-14 00:26:16 +0300

    Add new storybook backgrounds and viewports

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { GlobalStyle } from '../pages/_app';
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
@@ -17,6 +18,27 @@ export const decorators = [
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  layout: 'centered',
+  backgrounds: {
+    default: 'darkMain',
+    values: [
+      {
+        name: 'light',
+        value: '#ffffff',
+      },
+      {
+        name: 'darkMain',
+        value: '#020202;',
+      },
+      {
+        name: 'darkAdditional',
+        value: '#1A1A1A;',
+      },
+    ],
+  },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
