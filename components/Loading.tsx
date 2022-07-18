bdb1006a5a994c309535f8a3679af370a5a15e20
Ihor Kubrysh <ihorku@dom.money>
2022-07-18 17:33:30 +0300

    Add simple Loading Component

---
+++ components/Loading.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  color: white;
+`;
+
+const Loading = () => {
+  return (
+    <Wrapper>
+      <Title>
+        Loading...
+      </Title>
+    </Wrapper>
+  );
+};
+
+export default Loading;
