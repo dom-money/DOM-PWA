8c21626b6bd35444df5775df70ec59d69289fb5f
Ihor Kubrysh <ihorku@dom.money>
2022-07-20 16:37:32 +0300

    Add Header with Go Back Button Component & stories

---
+++ components/HeaderGoBack.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import IconButtonCircular from './IconButtonCircular';
+import GoBackIcon from '../styles/icons/GoBackIcon';
+
+interface HeaderGoBackProps {
+  /**
+   * Prop for extending styled-components style
+   */
+  className?: string;
+  /**
+   * URL to Click
+   */
+  href: string;
+}
+
+const Wrapper = styled.header`
+  display: flex;
+  align-items: center;
+  height: 2.75rem;
+`;
+
+const HeaderGoBack = ({ href, ...props }: HeaderGoBackProps) => {
+  return (
+    <Wrapper {...props}>
+      <IconButtonCircular
+        data-testid='goBackIcon'
+        href={href}
+      >
+        <GoBackIcon color='#F8F8F8'/>
+      </IconButtonCircular>
+    </Wrapper>
+  );
+};
+
+export default HeaderGoBack;
