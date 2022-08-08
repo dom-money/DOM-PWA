1edd9a3cbca81706edda0f92057ce658a2a287f3
Ihor Kubrysh <ihorku@dom.money>
2022-08-08 20:07:29 +0300

    Add Payment Status Component & stories

---
+++ components/PaymentStatus.tsx
@@ -0,0 +1,286 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import styled from 'styled-components';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+
+import Title from './Title';
+import Button from './Button';
+import PaymentStatusSuccessSVG from '../styles/svg/PaymentStatusSuccessSVG';
+import PaymentStatusFailedSVG from '../styles/svg/PaymentStatusFailedSVG';
+
+type PaymentStatusType = 'successful' | 'failed';
+
+interface PaymentStatusProps {
+  /**
+   * Type of Payment Status
+   */
+  type: PaymentStatusType;
+  /**
+   * Is Drawer Open?
+   */
+  isOpen: boolean;
+  /**
+   * On Drawer Close Handler Function
+   */
+  onClose?: () => void;
+  /**
+   * Receiver of the payment
+   */
+  paymentTo: string;
+  /**
+   * Amount of the payment
+   */
+  amount: number;
+  /**
+   * Payment message
+   */
+  message?: string;
+  /**
+   * Error message
+   */
+  errorMessage?: string;
+};
+
+interface BackdropProps {
+  /**
+   * Type of Payment Status
+   */
+  type: PaymentStatusType;
+  /**
+   * Bottom margin value for Backdrop Content
+   */
+  marginBottom: number;
+};
+
+const iOS =
+  typeof navigator !== 'undefined' &&
+  /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+const ColoredAreaWrapper = styled.div`
+  background-color: #1F1F1F;
+`;
+
+const Puller = styled.div`
+  width: 2.5rem;
+  height: 0.25rem;
+  margin-inline: auto;
+  margin-top: 0.75rem;
+  background-color: #ffffff;
+  opacity: 0.2;
+  border-radius: 6px;
+`;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-inline: 1.25rem;
+  margin-top: 1.5rem;
+`;
+
+const StatusTitle = styled.h1<{type: PaymentStatusType}>`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.5rem;
+  text-transform: capitalize;
+  margin-inline: auto;
+  margin-top: 0;
+  margin-bottom: 2rem;
+  color: ${(props) => {
+    if (props.type === 'successful') {
+      return props.theme.colors.success;
+    };
+    if (props.type === 'failed') {
+      return props.theme.colors.danger;
+    };
+  }};
+`;
+
+const DataContainer = styled.ul`
+  list-style-type: none;
+  margin: 0 0 1.875rem;
+  padding: 0;
+`;
+
+const DataItem = styled.li`
+  width: 100%;
+`;
+
+const TitleWithMargin = styled(Title)`
+  margin-bottom: 0.375rem;
+`;
+
+const Text = styled.p`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.125rem;
+  text-transform: capitalize;
+  color: #FFFFFF;
+  margin: 0;
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  };
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+`;
+
+const Divider = styled.li`
+  border-style: solid;
+  border-bottom-width: 0.6px;
+  border-top-width: 0;
+  border-left-width: 0;
+  border-right-width: 0;
+  margin-top: 0.75rem;
+  margin-bottom: 1rem;
+  border-color: #FFFFFF;
+  opacity: 0.2;
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  gap: 0.375rem;
+  padding: 0.313rem;
+  @media screen and (min-height: 700px) {
+    margin-bottom: 4.688rem;
+  }
+`;
+
+const BackdropContent = styled.div<{marginBottom: number}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${(props) => props.marginBottom && `margin-bottom: ${props.marginBottom}px`};
+`;
+
+const Backdrop = ({ type, ...props }: BackdropProps) =>
+  <BackdropContent {...props} >
+    {type === 'successful' &&
+        <PaymentStatusSuccessSVG />
+    }
+    {type === 'failed' &&
+        <PaymentStatusFailedSVG />
+    }
+  </BackdropContent>;
+
+const PaymentStatus = ({
+  type,
+  isOpen,
+  onClose = () => {},
+  paymentTo,
+  amount,
+  message,
+  errorMessage,
+}: PaymentStatusProps) => {
+  const [
+    drawerPaperHeight,
+    setDrawerPaperHeight,
+  ] = useState(0);
+
+  const [ windowHeight, setWindowHeight ] = useState(0);
+
+  // Catching window resize
+  useEffect(() => {
+    const updateWindowDimensions = () => {
+      const newWindowHeight = window.innerHeight;
+      setWindowHeight(newWindowHeight);
+    };
+
+    window.addEventListener('resize', updateWindowDimensions);
+    return () => window.removeEventListener('resize', updateWindowDimensions);
+  });
+
+  // Getting Paper's height
+  const drawerPaperRef = useCallback((node) => {
+    if (node === null) {
+      return;
+    };
+    setDrawerPaperHeight(node.children[ 2 ].clientHeight);
+  }, [ windowHeight ]);
+
+  return (
+    <SwipeableDrawer
+      ref={drawerPaperRef}
+      anchor={'bottom'}
+      open={isOpen}
+      onClose={onClose}
+      onOpen={() => {}}
+      disableBackdropTransition={!iOS}
+      disableDiscovery={true}
+      disableSwipeToOpen={true}
+      ModalProps={{
+        componentsProps: { backdrop: {
+          children: <Backdrop type={type} marginBottom={drawerPaperHeight} />,
+        } },
+      }}
+      sx={{
+        '.MuiDrawer-paper': {
+          backgroundColor: 'transparent',
+          borderRadius: '32px 32px 0px 0px',
+          maxHeight: '75%',
+          marginInline: 'auto',
+          maxWidth: 'fit-content',
+        },
+        '.MuiBackdrop-root': {
+          display: 'flex',
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          backdropFilter: 'blur(5px)',
+        },
+      }}
+    >
+      <ColoredAreaWrapper>
+        <Puller />
+        <ContentWrapper>
+          <StatusTitle type={type}>
+            {type === 'successful' && 'Successful'}
+            {type === 'failed' && 'Payment Failed'}
+          </StatusTitle>
+          <DataContainer>
+            <DataItem>
+              <TitleWithMargin text='Your Payment To' />
+              <Text>
+                {paymentTo}
+              </Text>
+            </DataItem>
+            <Divider />
+            <DataItem>
+              <TitleWithMargin text='Amount' />
+              <Text>
+                ${amount}
+              </Text>
+            </DataItem>
+            <Divider />
+            {type === 'successful' &&
+                <DataItem>
+                  <TitleWithMargin text='Message' />
+                  <Text>
+                    {message}
+                  </Text>
+                </DataItem>
+            }
+            {type === 'failed' &&
+              <DataItem>
+                <TitleWithMargin text='Error' />
+                <Text>
+                  {errorMessage}
+                </Text>
+              </DataItem>
+            }
+          </DataContainer>
+        </ContentWrapper>
+      </ColoredAreaWrapper>
+      {type === 'successful' &&
+        <ButtonsContainer>
+          <Button primary label='Send Again'/>
+          <Button label='Next'/>
+        </ButtonsContainer>
+      }
+      {type === 'failed' &&
+        <ButtonsContainer>
+          <Button primary label='Try Again'/>
+          <Button label='Home'/>
+        </ButtonsContainer>
+      }
+    </SwipeableDrawer>
+  );
+};
+
+export default PaymentStatus;
---
+++ stories/PaymentStatus.stories.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { useArgs } from '@storybook/client-api';
+
+import PaymentStatus from '../components/PaymentStatus';
+
+import SendToWalletPageRender from '../components/SendToWalletPageRender';
+
+export default {
+  title: 'Components/Payment Status',
+  component: PaymentStatus,
+  argTypes: {
+    type: {
+      type: { name: 'string', required: true },
+      description: 'Type of Payment Status',
+      table: {
+        type: {
+          summary: 'string',
+        },
+      },
+      options: [ 'successful', 'failed' ],
+      control: {
+        type: 'select',
+      },
+    },
+  },
+  parameters: {
+    layout: 'fullscreen',
+    viewport: {
+      defaultViewport: 'iphone12promax',
+    },
+  },
+} as ComponentMeta<typeof PaymentStatus>;
+
+const Template: ComponentStory<typeof PaymentStatus> = (args) => {
+  const [ { isOpen }, updateArgs ] = useArgs();
+
+  const handleDrawerClose = () => {
+    updateArgs({ isOpen: false });
+  };
+
+  return (
+    <div>
+      <SendToWalletPageRender
+        totalAmount={45725.06}
+        inputAmount='10000'
+        inputAddress='0xeA2a9ca3d52BEF67Cf562B59c5709B32Ed4c0eca'
+        areInputsValid={true}
+      />
+      <PaymentStatus
+        {...args}
+        isOpen={isOpen}
+        onClose={handleDrawerClose}
+      />
+    </div>
+  );
+};
+
+export const Successful = Template.bind({});
+Successful.args = {
+  type: 'successful',
+  isOpen: true,
+  paymentTo: '0x64ff637fb478863b7468bc97d30a5bf3a415fAb3',
+  amount: 200,
+  message: 'Submitted successfully',
+};
+
+export const Failed = Template.bind({});
+Failed.args = {
+  type: 'failed',
+  isOpen: true,
+  paymentTo: '0x64ff637fb478863b7468bc97d30a5bf3a415fAb3',
+  amount: 200,
+  errorMessage: 'Insufficient funds',
+};
