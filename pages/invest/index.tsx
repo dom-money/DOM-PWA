3a48128aa686da3c70c8305380f595066ae1ce91
Ihor Kubrysh <ihorku@dom.money>
2022-08-12 02:40:14 +0300

    Add loading & displaying payment status logic

--- pages/invest/index.tsx
+++ pages/invest/index.tsx
@@ -1,40 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import type { NextPage } from 'next';
 
 import InvestPageRender from '../../components/InvestPageRender';
+import PaymentStatus from '../../components/PaymentStatus';
 
 import useUSDCBalance from '../../hooks/useUSDCBalance';
 import useInputAmount from '../../hooks/useInputAmount';
+import useContract from '../../hooks/useContract';
 
 const InvestPage: NextPage = () => {
   const [
     walletBalance,
+    ,
     isWalletBalanceLoading,
     hasWalletBalanceError,
   ] = useUSDCBalance();
 
   const [
     inputAmount,
+    inputAmountUnformatted,
     inputAmountIsValid,
     inputAmountErrorMessage,
     inputAmountHandleChange,
     inputAmountHandleClear,
   ] = useInputAmount(walletBalance);
 
+  const [
+    depositToWealth,
+    depositResult,
+    isTransactionLoading,
+    depositErrorMessage,
+    handleUseDepositStateClear,
+  ] = useContract();
+
+  const [ isPaymentStatusOpen, setIsPaymentStatusOpen ] = useState(false);
+
+  useEffect(() => {
+    if (depositResult || depositErrorMessage) {
+      setIsPaymentStatusOpen(true);
+    }
+  }, [ depositResult, depositErrorMessage ]);
+
+  const handleInvestToWealth = () => {
+    depositToWealth(inputAmountUnformatted);
+  };
+
+  const handlePaymentStatusDrawerClose = () => {
+    setIsPaymentStatusOpen(false);
+  };
+
+  const handlePaymentStatusDrawerOnExited = () => {
+    inputAmountHandleClear();
+    handleUseDepositStateClear();
+  };
+
   if (isWalletBalanceLoading || hasWalletBalanceError) {
     return null;
   };
 
-
   return (
-    <InvestPageRender
-      totalAmount={walletBalance}
-      inputAmount={inputAmount}
-      onInputChange={inputAmountHandleChange}
-      errorMessage={inputAmountErrorMessage}
-      isInputValid={inputAmountIsValid}
-      clearButtonOnClick={inputAmountHandleClear}
-    />
+    <>
+      <InvestPageRender
+        totalAmount={walletBalance}
+        inputAmount={inputAmount}
+        onInputChange={inputAmountHandleChange}
+        errorMessage={inputAmountErrorMessage}
+        isInputValid={inputAmountIsValid}
+        isSubmitting={isTransactionLoading}
+        investButtonOnClick={handleInvestToWealth}
+        clearButtonOnClick={inputAmountHandleClear}
+      />
+      <PaymentStatus
+        type={depositResult ? 'successful' : 'failed'}
+        isOpen={isPaymentStatusOpen}
+        onClose={handlePaymentStatusDrawerClose}
+        onExited={handlePaymentStatusDrawerOnExited}
+        paymentTo='Your wealth wallet'
+        amount={inputAmount}
+        message='Submitted successfully'
+        errorMessage={depositErrorMessage ? depositErrorMessage : undefined}
+      />
+    </>
   );
 };
 
