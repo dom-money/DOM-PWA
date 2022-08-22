61eaa13447e5249546adf84d63e71a02e0f1df74
Ihor Kubrysh <ihorku@dom.money>
2022-08-22 12:33:34 +0000

    Small fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -49,19 +49,19 @@ const App = ({ Component, pageProps }: AppProps) => {
 
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/icons/icon-16x16.png"
           rel="icon"
           type="image/png"
           sizes="16x16"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          href="/icons/icon-32x32.png"
           rel="icon"
           type="image/png"
           sizes="32x32"
         />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content={theme.colors.primary} />
+        <meta name="theme-color" content={theme.colors.backgroundMain} />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
