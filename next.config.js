d5bc2eab717b9a1ac9fc9709dec65699b392a082
Ihor Kubrysh <ihorku@dom.money>
2023-04-18 22:39:00 +0300

    Disable ignoring ESLint when building next js app

--- next.config.js
+++ next.config.js
@@ -4,9 +4,4 @@ const withPWA = require('next-pwa')({
 
 module.exports = withPWA({
   // next.js config
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 });
