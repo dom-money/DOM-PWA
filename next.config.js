5e24cb99061cfe05a7a88815fe0f6e4a2839ca19
Ihor Kubrysh <ihorku@dom.money>
2023-05-24 03:19:16 +0300

    Migrate from Babel to SWC

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+/** @type {import('next').NextConfig} */
+
 const withPWA = require('next-pwa')({
   dest: 'public',
 });
 
 module.exports = withPWA({
-  // next.js config
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
 });
